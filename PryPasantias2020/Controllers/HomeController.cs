4f1ff821a5fd3fbe2081b72fae1b39944b2236ac
Chris <paul19992012@gmail.com>
2020-08-11 11:02:21 -0500

    Proyecto Inicial

---
+++ PryPasantias2020/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PryPasantias2020.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Acceder()
+        {
+            return View("Login");
+        }
+        public ActionResult Registrarse()
+        {
+            return View("Register");
+        }
+        public ActionResult RegistrarseAdmin()
+        {
+            return View("RegisterAdmin");
+        }
+    }
+}
